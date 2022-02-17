c9e1a0231cc77e00c23e57974921ac96ec6b4055
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-02-17 16:47:11 -0300

    LineEquation challenge modified (teacher solution)

--- Assets/Scripts/2_Line/LineEquation.cs
+++ Assets/Scripts/2_Line/LineEquation.cs
@@ -3,53 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteAlways]
 public class LineEquation : MonoBehaviour
 {
     [SerializeField] private Transform a;
     [SerializeField] private Transform b;
     [SerializeField] private Text equationLine;
+    private Vector2 A => a.position;
+    private Vector2 B => b.position;
+    private const float pointSize = 0.1f;
 
-    private float tangent;
-    private float c;
-    private float angle;
-
-    private void Update() 
+    private void CalculateEquationOfLine(out float m, out float c)
     {
-        CalculateEquationOfLine();
-    }
-
-    private void LateUpdate() 
-    {
-        equationLine.text = $"y = {tangent}x + {c} (Angle => {angle})";
-    }
-
-    private void CalculateEquationOfLine()
-    {
-        var oppositeSide = b.position.y - a.position.y;
-        var adjacentSide = b.position.x - a.position.x;
-        tangent = oppositeSide / adjacentSide;
-        c = -tangent * a.position.x + a.position.y;
-
-        CalculateAngle(oppositeSide, adjacentSide);
-    }
-
-    private void CalculateAngle(float oppositeSide, float adjacentSide)
-    {
-        var hypotenuse = Mathf.Sqrt(oppositeSide * oppositeSide + adjacentSide * adjacentSide);
-        var sinAngle = oppositeSide / hypotenuse;
-        angle = Mathf.Asin(sinAngle) * Mathf.Rad2Deg;
+        m = (B.y - A.y) / (B.x - A.x);
+        c = -m * A.x + A.y;
     }
 
     private void OnDrawGizmos() 
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(a.position, 0.1f);
-        Gizmos.DrawSphere(b.position, 0.1f);
-
-        Gizmos.DrawLine(a.position, b.position);
+        Gizmos.DrawSphere(A, pointSize);
+        Gizmos.DrawSphere(B, pointSize);
+        Gizmos.DrawLine(A, B);
 
         Gizmos.color = Color.white;
-        Gizmos.DrawLine(a.position, new Vector2(b.position.x, a.position.y));
+        Gizmos.DrawLine(A, new Vector2(B.x, A.y));
+
+        CalculateEquationOfLine(out var m, out var c);
+        var angle = Mathf.Atan(m) * Mathf.Rad2Deg;
+        equationLine.text = $"y = {m}x + {c} (Angle => {angle})";
     }
 }
