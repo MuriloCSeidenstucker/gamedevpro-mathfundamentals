5eabf45686df0e9d0c688454b4cbcd51073de349
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-02-25 15:22:45 -0300

    VectorOperations challenge completed (my solution)

--- Assets/Scripts/6_VectorOperations/VectorVisualizer.cs
+++ Assets/Scripts/6_VectorOperations/VectorVisualizer.cs
@@ -1,21 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+enum Operations
+{
+    None,
+    Add,
+    Scale,
+    Normalize
+}
 
 public class VectorVisualizer : MonoBehaviour
 {
-    private const float sizeCube = 0.1f;
+    [Header("UI Text")]
+    [SerializeField] private Text valuesText;
 
-    private void OnDrawGizmos() 
+    [Header("Values")]
+    [SerializeField] private MyVector3 v;
+    [SerializeField] private MyVector3 w;
+    [SerializeField] private float a;
+    
+    [Space]
+    [SerializeField] private Operations operation;
+
+    private MyVector3 origin => (MyVector3)transform.position;
+
+    private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        GizmosUtils.DrawVector(transform.position, MyVector3.Right*2);
-        Gizmos.color = Color.green;
-        GizmosUtils.DrawVector(transform.position, MyVector3.Up*2);
-        Gizmos.color = Color.blue;
-        GizmosUtils.DrawVector(transform.position, MyVector3.Forward*2);
+        DrawVector(origin, v, Color.yellow);
 
+        switch (operation)
+        {
+            case Operations.None:
+                DrawVectorsMontage();
+                break;
+            case Operations.Add:
+                DrawVectorSum();
+                break;
+            case Operations.Scale:
+                DrawVectorScale();
+                break;
+            case Operations.Normalize:
+                DrawVectorNormalized();
+                break;
+            default:
+                Debug.LogError($"Operação {operation} escolhida é inválida!");
+                break;
+        }
+
+        DrawCoordinates();
+    }
+
+    private void DrawVectorsMontage()
+    {
+        MyVector3 meetingPoint = new MyVector3(v.X, 0, v.Z);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new MyVector3(v.X, 0, 0), meetingPoint);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(new MyVector3(0, 0, v.Z), meetingPoint);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(v, meetingPoint);
+        valuesText.text = $"{v.Magnitude}";
+    }
+
+    private void DrawVectorSum()
+    {
+        var r = v + w;
+        DrawVector(v, w, Color.gray);
+        DrawVector(origin, r, Color.cyan);
+        valuesText.text = $"Result (x: {r.X}, y: {r.Y}, z: {r.Z})";
+    }
+
+    private void DrawVectorScale()
+    {
+        var r = v * a;
+        DrawVector(origin, r, Color.cyan);
+        valuesText.text = $"Scaled Magnitude: {r.Magnitude}";
+    }
+
+    private void DrawVectorNormalized()
+    {
+        var r = v.Normalize;
+        DrawVector(origin, r, Color.cyan);
+        valuesText.text = $"Normalize Magnitude: {r.Magnitude}";
+    }
+
+    private void DrawVector(MyVector3 from, MyVector3 to, Color color)
+    {
+        Gizmos.color = color;
+        GizmosUtils.DrawVector(from, to);
+    }
+
+    private void DrawCoordinates()
+    {
+        var lineScale = 5;
+        float sizeCube = 0.1f;
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(origin, MyVector3.Right * lineScale*2);
+        Gizmos.DrawLine(origin, MyVector3.Up * lineScale*2);
+        Gizmos.DrawLine(origin, MyVector3.Forward * lineScale*2);
+
+        Gizmos.color = Color.red;
+        GizmosUtils.DrawVector(origin, MyVector3.Right * lineScale);
+        Gizmos.color = Color.green;
+        GizmosUtils.DrawVector(origin, MyVector3.Up * lineScale);
+        Gizmos.color = Color.blue;
+        GizmosUtils.DrawVector(origin, MyVector3.Forward * lineScale);
         Gizmos.color = Color.gray;
-        Gizmos.DrawCube(transform.position, MyVector3.One*sizeCube);
+        Gizmos.DrawCube(origin, MyVector3.One * sizeCube);
     }
 }
--- Assets/Scripts/Utils/MyVector3.cs
+++ Assets/Scripts/Utils/MyVector3.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public struct MyVector3
 {
     public float X;
@@ -16,11 +18,13 @@ public struct MyVector3
     }
 
     public static implicit operator Vector3(MyVector3 v) => new Vector3(v.X, v.Y, v.Z);
+    public static explicit operator MyVector3(Vector3 v) => new MyVector3(v.x, v.y, v.z);
 
     public static MyVector3 Right => new MyVector3(1, 0, 0);
     public static MyVector3 Up => new MyVector3(0, 1, 0);
     public static MyVector3 Forward => new MyVector3(0, 0, 1);
     public static MyVector3 One => new MyVector3(1, 1, 1);
+    public static MyVector3 Zero => new MyVector3(0, 0, 0);
 
     public float Magnitude => Mathf.Sqrt((X*X)+(Y*Y)+(Z*Z));
 
@@ -28,7 +32,7 @@ public struct MyVector3
     {
         get
         {
-            var u = new MyVector3(0,0,0);
+            var u = MyVector3.Zero;
             u.X = this.X * (1/this.Magnitude);
             u.Y = this.Y * (1/this.Magnitude);
             u.Z = this.Z * (1/this.Magnitude);
@@ -36,6 +40,8 @@ public struct MyVector3
         }
     }
 
+    public static MyVector3 operator -(MyVector3 a)
+        => new MyVector3(-a.X, -a.Y, -a.Z);
     public static MyVector3 operator +(MyVector3 a, MyVector3 b)
         => new MyVector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
 
