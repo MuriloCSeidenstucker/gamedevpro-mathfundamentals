aeae8f4ca2c5cfbec4da958a45fd00acdd4daf08
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-03-02 15:29:14 -0300

    Matrix2x2 struct created (my solution)

---
+++ Assets/Scripts/Utils/Matrix2x2.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Matrix2x2
+{
+    // First column.
+    public float IX;
+    public float IY;
+    // Second column.
+    public float JX;
+    public float JY;
+
+    public Matrix2x2(float iX, float iY, float jX, float jY)
+    {
+        IX = iX;
+        IY = iY;
+        JX = jX;
+        JY = jY;
+    }
+
+    public static Matrix2x2 Identity => new Matrix2x2(1, 0, 0, 1);
+
+    public Vector2 VectorMultiplier(Matrix2x2 m, Vector2 v)
+    {
+        return new Vector2((m.IX*v.x) + (m.JX*v.y), (m.IY*v.x) + (m.JY*v.y));
+    }
+
+    public Vector2 MatrixMultiplier(Matrix2x2[] m, Vector2 v)
+    {
+        var mR = Matrix2x2.Identity;
+        if (m.Length > 0)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                var cM = m[i];
+                var a = (cM.IX*mR.IX) + (cM.JX*mR.IY);
+                var b = (cM.IY*mR.IX) + (cM.JY*mR.IY);
+                var c = (cM.IX*mR.JX) + (cM.JX*mR.JY);
+                var d = (cM.IY*mR.JX) + (cM.JY*mR.JY);
+                mR = new Matrix2x2(a, b, c, d);
+            }
+        }
+        return VectorMultiplier(mR, v);
+    }
+}
