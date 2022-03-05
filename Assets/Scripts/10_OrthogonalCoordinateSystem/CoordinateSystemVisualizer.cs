30563ea28f9535cc2d97506f3e046efe589bfb0f
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-03-05 18:00:25 -0300

    Modified OrthogonalCoordinateSystem challenge solution based on the teacher's solution

--- Assets/Scripts/10_OrthogonalCoordinateSystem/CoordinateSystemVisualizer.cs
+++ Assets/Scripts/10_OrthogonalCoordinateSystem/CoordinateSystemVisualizer.cs
@@ -17,32 +17,45 @@ public class CoordinateSystemVisualizer : MonoBehaviour
 
     private Vector3 Origin => transform.position;
     private Vector3 n;
+    private Vector3 copyV;
+    private Vector3 copyW;
     private const float vectorThickness = 2.0f;
 
     private void OnDrawGizmos() 
     {
-        Gizmos.color = Color.yellow;
-        var forcedV = forceOrthogonalSystem ? v.normalized : v;
-        GizmosUtils.DrawVector(Origin, forcedV, vectorThickness);
-        Gizmos.color = Color.cyan;
-        var forcedW = forceOrthogonalSystem ? w.normalized : w;
-        GizmosUtils.DrawVector(Origin, forcedW, vectorThickness);
+        copyV = v;
+        copyW = w;
 
         switch (handedness)
         {
             case Handedness.left:
                 n = Vector3.Cross(v, w);
+                ForceOrthogonalSystem(handedness);
                 break;
             case Handedness.right:
                 n = Vector3.Cross(w, v);
+                ForceOrthogonalSystem(handedness);
                 break;
             default:
                 Debug.LogError($"Opção {handedness} inválida!");
                 break;
         }
 
+        Gizmos.color = Color.yellow;
+        GizmosUtils.DrawVector(Origin, copyV, vectorThickness);
+        Gizmos.color = Color.cyan;
+        GizmosUtils.DrawVector(Origin, copyW, vectorThickness);
         Gizmos.color =  Color.magenta;
-        var forcedN = forceOrthogonalSystem ? n.normalized : n;
-        GizmosUtils.DrawVector(Origin, forcedN, vectorThickness);
+        GizmosUtils.DrawVector(Origin, n, vectorThickness);
+    }
+
+    private void ForceOrthogonalSystem(Handedness handedness)
+    {
+        if (forceOrthogonalSystem)
+        {
+            copyV.Normalize();
+            n.Normalize();
+            copyW = handedness == Handedness.left ? Vector3.Cross(n, copyV).normalized : Vector3.Cross(copyV, n).normalized;
+        }
     }
 }
