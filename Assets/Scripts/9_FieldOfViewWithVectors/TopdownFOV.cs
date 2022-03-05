ec530d8a2400c5047441d93d1bb1007fa09df07b
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-03-05 15:33:15 -0300

    Modified FieldOfViewWithVectors challenge solution based on the teacher's solution

--- Assets/Scripts/9_FieldOfViewWithVectors/TopdownFOV.cs
+++ Assets/Scripts/9_FieldOfViewWithVectors/TopdownFOV.cs
@@ -10,50 +10,36 @@ public class TopdownFOV : MonoBehaviour
 
     private Vector3 Player => player.position;
     private Vector3 Origin => transform.position;
-    private Vector3 LeftDir => Quaternion.Euler(0, -angle * 0.5f, 0) * transform.forward;
-    private Vector3 RightDir => Quaternion.Euler(0, angle * 0.5f, 0) * transform.forward;
 
     private void OnDrawGizmos() 
     {
+        var leftDir = Quaternion.Euler(0, -angle * 0.5f, 0) * transform.forward;
+        var rightDir = Quaternion.Euler(0, angle * 0.5f, 0) * transform.forward;
+
         Gizmos.color = CanSeeTarget(Player) ? Color.green : Color.white;
         Gizmos.DrawWireSphere(Origin, radius);
-        Gizmos.DrawRay(Origin, LeftDir * radius);
-        Gizmos.DrawRay(Origin, RightDir * radius);
-
+        Gizmos.DrawRay(Origin, leftDir.normalized * radius);
+        Gizmos.DrawRay(Origin, rightDir.normalized * radius);
     }
 
     private bool CanSeeTarget(Vector3 target)
     {
-        var v = Origin + RightDir * radius;
-        var w = Origin + LeftDir * radius;
+        var toTarget = target - Origin;
 
-        if (target.magnitude > v.magnitude)
+        if (toTarget.sqrMagnitude > radius * radius)
         {
             return false;
         }
 
-        var dotTV = DotProduct(target, v);
-        var dotTW = DotProduct(target, w);
-        if (dotTV < 0 && dotTW < 0)
+        var dot = Vector3.Dot(toTarget, transform.forward);
+        if (dot <  0)
         {
             return false;
         }
 
-        var cosPV = FindCosine(target, v);
-        var cosPW = FindCosine(target, w);
-        var cosVW = FindCosine(v, w);
+        var cos = dot / (toTarget.magnitude * transform.forward.magnitude);
+        var angleToTarget = Mathf.Acos(cos) * Mathf.Rad2Deg;
 
-        return cosPV >= cosVW && cosPW >= cosVW;
-    }
-
-    private float DotProduct(Vector3 v, Vector3 w)
-    {
-        return (v.x * w.x) + (v.y * w.y) + (v.z * w.z);
-    }
-
-    private float FindCosine(Vector3 v, Vector3 w)
-    {
-        var dotVW = DotProduct(v, w);
-        return dotVW / (v.magnitude * w.magnitude);
+        return angleToTarget <= (angle * 0.5f);
     }
 }
