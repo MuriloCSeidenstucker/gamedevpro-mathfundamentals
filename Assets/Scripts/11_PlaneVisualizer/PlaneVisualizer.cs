68aa871ed2714c7eb71b68c67206111dee2ba3b6
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-03-11 09:39:47 -0300

    PlaneVisualizer challenge completed (my solution)

--- Assets/Scripts/11_PlaneVisualizer/PlaneVisualizer.cs
+++ Assets/Scripts/11_PlaneVisualizer/PlaneVisualizer.cs
@@ -2,38 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum PlaneOperations
+{
+    None, IsInFront, ProjectPoint, ProjectVector
+}
+
 public class PlaneVisualizer : MonoBehaviour
 {
-    [SerializeField] private MyPlane myPlane;
+    [SerializeField] private Vector3 p1;
+    [SerializeField] private Vector3 p2;
+    [SerializeField] private Vector3 p3;
+    [SerializeField] private Vector3 point;
+    [SerializeField] private PlaneOperations operations;
 
-    private Vector3 v => myPlane.P3 - myPlane.P1;
-    private Vector3 w => myPlane.P2 - myPlane.P1;
     private Vector3 Origin => transform.position;
+    private MyPlane myPlane;
 
     private const float radiusSphere = 0.1f;
     private const float vectorThickness = 1.0f;
 
     private void OnDrawGizmos() 
     {
-        var n = Vector3.Cross(v, w).normalized;
+        myPlane = new MyPlane(p1, p2, p3);
+        var n = myPlane.Normal;
 
         var projectP1N = (Vector3.Dot(myPlane.P1, n) * n);
 
+        switch (operations)
+        {
+            case PlaneOperations.IsInFront:
+                Gizmos.color = myPlane.IsInFront(point) ? Color.green : Color.red;
+                Gizmos.DrawSphere(point, radiusSphere);
+                break;
+            case PlaneOperations.ProjectPoint:
+                ProjectPoint();
+                break;
+            case PlaneOperations.ProjectVector:
+                ProjectVector();
+                break;
+            default:
+                break;
+        }
 
         Gizmos.color = Color.magenta;
-        GizmosUtils.DrawVector(myPlane.P1, n, vectorThickness);
+        GizmosUtils.DrawVector(p1, n, vectorThickness);
 
         Gizmos.color = Color.white;
-        Gizmos.DrawSphere(myPlane.P1, radiusSphere);
-        Gizmos.DrawSphere(myPlane.P2, radiusSphere);
-        Gizmos.DrawSphere(myPlane.P3, radiusSphere);
+        Gizmos.DrawSphere(p1, radiusSphere);
+        Gizmos.DrawSphere(p2, radiusSphere);
+        Gizmos.DrawSphere(p3, radiusSphere);
         GizmosUtils.DrawVector(Origin, projectP1N, vectorThickness);
 
         GizmosUtils.DrawPlane(n, projectP1N, Vector2.one*10);
-        DrawBase();
+        DrawCoordinates();
     }
 
-    private void DrawBase()
+    private void ProjectPoint()
+    {
+        var q = myPlane.ProjectPoint(point, p1, out var projectVN);
+        Gizmos.color = Color.white;
+        Gizmos.DrawRay(point, -projectVN);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(point, radiusSphere);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(q, radiusSphere);
+        GizmosUtils.DrawVector(Origin, q, vectorThickness);
+    }
+
+    private void ProjectVector()
+    {
+        var projectV = myPlane.ProjectVector(point, out var projectVN);
+        Gizmos.color = Color.white;
+        Gizmos.DrawRay(point + p1, -projectVN);
+        Gizmos.color = Color.cyan;
+        GizmosUtils.DrawVector(p1, point, vectorThickness);
+        Gizmos.color = Color.yellow;
+        GizmosUtils.DrawVector(p1, projectV, vectorThickness);
+    }
+
+    private void DrawCoordinates()
     {
         Gizmos.color = Color.red;
         GizmosUtils.DrawVector(Origin, Vector3.right);
--- Assets/Scripts/Utils/MyPlane.cs
+++ Assets/Scripts/Utils/MyPlane.cs
@@ -8,11 +8,39 @@ public struct MyPlane
     public Vector3 P1;
     public Vector3 P2;
     public Vector3 P3;
+    public Vector3 V;
+    public Vector3 W;
 
     public MyPlane(Vector3 p1, Vector3 p2, Vector3 p3)
     {
         P1 = p1;
         P2 = p2;
         P3 = p3;
+
+        V = P2 - P1;
+        W = P3 - P1;
+    }
+
+    public Vector3 Normal => Vector3.Cross(V, W).normalized;
+    public float distFromOrigin => Vector3.Dot(Normal, P1);
+
+    public bool IsInFront(Vector3 point)
+    {
+        var dot = Vector3.Dot(point, Normal);
+        return (dot - distFromOrigin) > 0;
+    }
+
+    public Vector3 ProjectPoint(Vector3 anyPoint, Vector3 pointInPlane, out Vector3 projVN)
+    {
+        var v = anyPoint - pointInPlane;
+        var projectVector = ProjectVector(v, out var projectVN);
+        projVN = projectVN;
+        return pointInPlane + projectVector;
+    }
+
+    public Vector3 ProjectVector(Vector3 v, out Vector3 projectVN)
+    {
+        projectVN = (Vector3.Dot(Normal, v) * Normal);
+        return v - projectVN;
     }
 }
