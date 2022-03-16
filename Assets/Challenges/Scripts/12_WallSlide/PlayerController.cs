8fb310ff5a2cbd75b0ed0da27ab8ac16595852ca
Murilo C. Seidenstucker <murilocampos.004@gmail.com>
2022-03-16 13:57:15 -0300

    Base structure of the TopdownGameMovement challenge has been completed

---
+++ Assets/Challenges/Scripts/12_WallSlide/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMovement))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterMovement characterMovement;
+
+    private void Awake()
+    {
+        characterMovement = GetComponent<CharacterMovement>();
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        characterMovement.SetInput(horizontal, vertical);
+    }
+}
